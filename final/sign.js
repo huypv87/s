7daee6413ac86a59f5bf65a8a33e8b89cd689b3c
HuyPV <huyphungvan@vccorp.vn>
2016-04-26 15:56:54 +0700

    JS chung minh ban quyen

---
+++ final/sign.js
@@ -0,0 +1,11 @@
+/*
+Cam on ban da su dung template mien phi cua chung toi
+Website: http://ptsblogger.com/
+*/
+var hp_sign = '2932be849a29933ccef5b862813a7651';
+function hp_echo(msg) {
+	console.log(msg);
+}
+function hp_rediect(url) {
+	location.href = url;
+}
